68a5eb9f122ba8a07bf2e93d536de73b6a95af75
Guesmi Achref <guesmi.achraf3@gmail.com>
2023-08-25 20:01:51 +0200

    restAPI

---
+++ src/main/java/com/sip/ams/AmsRest2023Application.java
@@ -0,0 +1,13 @@
+package com.sip.ams;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmsRest2023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmsRest2023Application.class, args);
+	}
+
+}
